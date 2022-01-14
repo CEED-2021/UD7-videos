e4bcf4b05d9b434c16465bc13b87c233678f907b
Alvaro Maceda <alvaro@alvaromaceda.es>
2022-01-14 07:10:34 +0100

    adds examples

--- W2/03-promises-automatic_reject.js
+++ W2/03-promises-automatic_reject.js
@@ -48,10 +48,37 @@ asyncThing()
   }
 })
 .catch( (error) => {
-  console.log(`Detected an error: ${error}`);
+  try {
+    console.log(`Detected an error: ${error}`);
+  }catch(error){
+    return Promise.reject(error)
+  }
 })
 
 
 // -------------------------------------------------------------------------
-// TO-DO: for promises creation
+// The try-catch is appened to promises creation, too
 // -------------------------------------------------------------------------
+function asyncThing2({fail = false} = {}) {
+  return new Promise( (resolve, reject) => {
+    throw "Something terrible has happended";
+  })
+}
+
+function asyncThing2_real({fail = false} = {}) {
+  try {
+    return new Promise( (resolve, reject) => {
+      throw "Something terrible has happended";
+    })
+  } catch(error){
+      return Promise.reject(error)
+  }
+}
+
+asyncThing2()
+.then( () => {
+  console.log("This won't be executed")
+})
+.catch( (error) => {
+  console.log(`Detected an error: ${error}`);
+})
